5f00b4c519c64a1e0c7268a956b62373eafc6735
Elson D'Souza <elson1996@users.noreply.github.com>
2016-12-16 21:24:21 +0530

    Update server.js
    Corrected port number

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express'),
     pug = require('pug'),
     path = require('path');
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser')
+    port = process.env.PORT || 8080;
 
 var routes = require('./routes/index');
 var app = express();
+
+var server = app.listen(port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+  console.log("\nListening at http://%s:%s\n",host ,port);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -12,11 +20,5 @@ app.set('view engine', 'pug');
 
 app.use('/', routes);
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 module.exports = app;
